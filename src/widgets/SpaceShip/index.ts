e36c9e06eb1d29fe3b455d8df01c8ded4b480e53
sunub <bsc5672@gmail.com>
2025-10-15 23:16:12 +0900

    refactor : 모델에 관련된 파일구조 변경

--- src/widgets/SpaceShip/index.ts
+++
@@ -1,7 +0,0 @@
-export { SpaceShip } from "./model/SpaceShip";
-
-// Debug Modules
-export { SpaceShipPhysicsDebugModule } from "./debug/SpaceShip.PhysicsDebug";
-export { SpaceShipPositionDebugModule, type SpaceShipPositionDebugContext } from "./debug/SpaceShip.PositionDebug";
-export { SpaceShipCameraDebugModule } from "./debug/SpaceShip.CameraDebug";
-export { SpaceShipVisualDebugModule, type SpaceShipVisualDebugContext } from "./debug/SpaceShip.VisualDebug";
