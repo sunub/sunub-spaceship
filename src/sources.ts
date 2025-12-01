67a491fa31032254319be43277395d7f79523727
sunub <bsc5672@gmail.com>
2025-12-01 13:30:13 +0900

    feat(sources): 새로운 glTF 모델 추가

--- src/sources.ts
+++ src/sources.ts
@@ -13,5 +13,35 @@ export default [
     name: 'crystalStandModel',
     type: 'gltfModel',
     path: '/models/crystal_stand.glb',
+  },
+  {
+    name: 'planet',
+    type: 'gltfModel',
+    path: '/models/planet.gltf',
+  },
+  {
+    name: 'planetBrightModel',
+    type: 'gltfModel',
+    path: '/models/planetBright.gltf',
+  },
+  {
+    name: 'atmosphereModel',
+    type: 'gltfModel',
+    path: '/models/atmosphere.gltf',
+  },
+  {
+    name: 'landModel',
+    type: 'gltfModel',
+    path: '/models/land.gltf',
+  },
+  {
+    name: 'treeLightsModel',
+    type: 'gltfModel',
+    path: '/models/tree_light.gltf',
+  },
+  {
+    name: 'crystalModel',
+    type: 'gltfModel',
+    path: '/models/crytal_with_land.gltf',
   }
 ];
