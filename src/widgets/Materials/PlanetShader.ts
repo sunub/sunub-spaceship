f8194cafb6db09f4ad24fbd82584982c00a9d16e
sunub <bsc5672@gmail.com>
2025-12-29 14:20:15 +0900

    refactor : Editor 설정 변경 및 포맷터 및 린터 추가로 인한 리펙토링 수행

---
+++ src/widgets/Materials/PlanetShader.ts
@@ -0,0 +1,7 @@
+import shaderFragment from "@/widgets/Shader/planet/fragment.glsl"
+import shaderVertex from "@/widgets/Shader/planet/vertext.glsl"
+
+export default {
+	vertexShader: shaderVertex,
+	fragmentShader: shaderFragment,
+}
