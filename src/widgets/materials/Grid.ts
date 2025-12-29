7107aa26ebdeff070252c9d5545548d66d03ded0
sunub <bsc5672@gmail.com>
2025-12-29 14:29:42 +0900

    refactor : 일관된 규칙을 갖게끔 폴더 구조및 파일명 변경

--- src/widgets/materials/Grid.ts
+++
@@ -1,7 +0,0 @@
-import shaderVertex from '@/widgets/Shader/grid/vertex.glsl'
-import shaderFragment from '@/widgets/Shader/grid/fragment.glsl'
-
-export default {
-    vertexShader: shaderVertex,
-    fragmentShader: shaderFragment
-}
--- src/widgets/materials/PlanetShader.ts
+++
@@ -1,7 +0,0 @@
-import shaderVertex from '@/widgets/Shader/planet/vertext.glsl';
-import shaderFragment from '@/widgets/Shader/planet/fragment.glsl';
-
-export default {
-    vertexShader: shaderVertex,
-    fragmentShader: shaderFragment
-}
