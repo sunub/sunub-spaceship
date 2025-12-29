e31ff09d30844c9ef2421562c3bffd1c84fb08d7
sunub <bsc5672@gmail.com>
2025-12-29 14:36:13 +0900

    chore : 프로젝트 환경 설정 및 일부 typescript, vite 설정 수정
    - Yarn Berry (PnP 또는 Zero-install) 기반의 패키지 관리 체계 구축
    - 고성능 린트/포맷 툴인 Biome 도입 및 프로젝트 규칙 정의
    - .editorconfig 를 통한 에디터 수준의 코드 컨벤션 강제

--- vite.config.ts
+++ vite.config.ts
@@ -1,56 +1,62 @@
-import path from "path";
-import { defineConfig } from "vite";
-import { visualizer } from "rollup-plugin-visualizer";
-import glsl from "vite-plugin-glsl";
+import path from "path"
+import { visualizer } from "rollup-plugin-visualizer"
+import { defineConfig } from "vite"
+import glsl from "vite-plugin-glsl"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [visualizer(), glsl()],
-  base: "/",
-  assetsInclude: ["**/*.glb", "**/*.hdr", "**/*.gltf", "**/*.bin"],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  publicDir: "public",
-  build: {
-    minify: "esbuild",
-    assetsDir: "assets",
-    modulePreload: {
-      polyfill: true,
-    },
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, "index.html"),
-      },
-      output: {
-        experimentalMinChunkSize: 30000,
-        chunkFileNames: "assets/js/[name]-[hash].js",
-        entryFileNames: "assets/js/[name]-[hash].js",
-        assetFileNames: (assetInfo) => {
-          const fileName = assetInfo.name ?? "unknown";
-          const extType = fileName.split(".").pop()?.toLowerCase() ?? "";
+	plugins: [visualizer(), glsl()],
+	base: "/",
+	assetsInclude: ["**/*.glb", "**/*.hdr", "**/*.gltf", "**/*.bin"],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	publicDir: "public",
+	server: {
+		host: "127.0.0.1",
+		port: 5173,
+		strictPort: true,
+	},
+	build: {
+		minify: "esbuild",
+		chunkSizeWarningLimit: 1000,
+		assetsDir: "assets",
+		modulePreload: {
+			polyfill: true,
+		},
+		rollupOptions: {
+			input: {
+				main: path.resolve(__dirname, "index.html"),
+			},
+			output: {
+				experimentalMinChunkSize: 30000,
+				chunkFileNames: "assets/js/[name]-[hash].js",
+				entryFileNames: "assets/js/[name]-[hash].js",
+				assetFileNames: (assetInfo) => {
+					const fileName = assetInfo.name ?? "unknown"
+					const extType = fileName.split(".").pop()?.toLowerCase() ?? ""
 
-          if (/glb|hdr|gltf|bin/i.test(extType)) {
-            return `assets/models/[name]-[hash][extname]`;
-          }
+					if (/glb|hdr|gltf|bin/i.test(extType)) {
+						return `assets/models/[name]-[hash][extname]`
+					}
 
-          return `assets/[name]-[hash][extname]`;
-        },
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("@react-three")) {
-              return "vendor-react-three";
-            } else if (id.includes("three")) {
-              return "vendor-three";
-            } else if (id.includes("@dimforge/rapier3d")) {
-              return "vendor-rapier3d";
-            }
-            return "vendor";
-          }
-        },
-      },
-    },
-  },
-});
+					return `assets/[name]-[hash][extname]`
+				},
+				manualChunks(id) {
+					if (id.includes("node_modules")) {
+						if (id.includes("@react-three")) {
+							return "vendor-react-three"
+						} else if (id.includes("three")) {
+							return "vendor-three"
+						} else if (id.includes("@dimforge/rapier3d")) {
+							return "vendor-rapier3d"
+						}
+						return "vendor"
+					}
+				},
+			},
+		},
+	},
+})
