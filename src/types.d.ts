059612a39a17b8c8b119a0e63c8c89f63672fa1c
sunub <bsc5672@gmail.com>
2026-01-22 19:38:56 +0900

    refactor : Biome 포맷 적용

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,6 +2,6 @@ import type { ShaderNodeObject } from "three/tsl"
 import type * as THREE from "three/webgpu"
 
 declare global {
-	type TSLNode<T extends THREE.Node> = ShaderNodeObject<T>
-	type UniformNode<T> = ShaderNodeObject<THREE.UniformNode<T>>
+    type TSLNode<T extends THREE.Node> = ShaderNodeObject<T>
+    type UniformNode<T> = ShaderNodeObject<THREE.UniformNode<T>>
 }
