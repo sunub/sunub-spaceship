65ae9ed820043d441325ee71494368cb7e1188f0
sunub <bsc5672@gmail.com>
2026-01-22 16:38:05 +0900

    refactor : 코드 스타일을 포맷 Biome 스타일에 맞춰 개선

--- src/widgets/Debug/index.ts
+++ src/widgets/Debug/index.ts
@@ -2,12 +2,19 @@ import { Pane } from "tweakpane"
 
 import type { PaneConfig } from "tweakpane/dist/types/pane/pane-config"
 
-export class Debug extends Pane
-{
-	public options: PaneConfig
-	constructor(options: PaneConfig)
-	{
-		super(options)
-		this.options = options
-	}
+export class Debug {
+    static instance: Pane | null = null
+
+    constructor(options: PaneConfig) {
+        if (!Debug.instance) {
+            Debug.instance = new Pane(options)
+        }
+    }
+
+    static getInstance(options: PaneConfig): Pane {
+        if (!Debug.instance) {
+            Debug.instance = new Pane(options)
+        }
+        return Debug.instance
+    }
 }
