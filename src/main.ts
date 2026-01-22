fce9056ea0923b6747f3a7b6a2b86b035df75732
sunub <bsc5672@gmail.com>
2026-01-22 19:36:26 +0900

    refactor : Biome 코드 포맷 적용 및 css 파일 이동

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
-import "./style.css"
 import { Game } from "./widgets/Game"
 
 const main = async () => {
-	const game = Game.getInstance()
-	await game.initialize()
-	game.start()
+    const game = Game.getInstance()
+    await game.init()
+    game.start()
 }
 
 main().catch(console.error)
