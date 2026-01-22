6034c1bb39c491fc02fa0a8cb2160770343eb6d5
sunub <bsc5672@gmail.com>
2026-01-22 16:48:32 +0900

    refactor : 코드 스타일을 포맷 Biome 스타일에 맞춰 개선

--- src/widgets/Materials/PlanetShader.ts
+++ src/widgets/Materials/PlanetShader.ts
@@ -2,6 +2,6 @@ import shaderFragment from "@/widgets/Shader/planet/fragment.glsl"
 import shaderVertex from "@/widgets/Shader/planet/vertext.glsl"
 
 export default {
-	vertexShader: shaderVertex,
-	fragmentShader: shaderFragment,
+    vertexShader: shaderVertex,
+    fragmentShader: shaderFragment,
 }
