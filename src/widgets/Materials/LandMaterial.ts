d50d17c1c835187583fb8b6e377ca126b181d1c6
sunub <bsc5672@gmail.com>
2026-01-22 16:48:21 +0900

    refactor : 코드 스타일을 포맷 Biome 스타일에 맞춰 개선

--- src/widgets/Materials/LandMaterial.ts
+++ src/widgets/Materials/LandMaterial.ts
@@ -1,51 +1,46 @@
 // @ts-nocheck
-import * as THREE from "three"
+import * as THREE from "three/webgpu"
 import { fragmentShader, vertexShader } from "../Shader/LandShader"
 
-interface LandMaterialOptions
-{
-	side?: THREE.Side
-	uLightPosition?: THREE.Vector3
-	uDarkColor?: THREE.Color
-	uLightColor?: THREE.Color
-	uLightIntensity?: number
-	uLightRadius?: number
+interface LandMaterialOptions {
+    side?: THREE.Side
+    uLightPosition?: THREE.Vector3
+    uDarkColor?: THREE.Color
+    uLightColor?: THREE.Color
+    uLightIntensity?: number
+    uLightRadius?: number
 }
 
-export class LandMaterial extends THREE.ShaderMaterial
-{
-	constructor({
-		side = THREE.FrontSide,
-		uLightPosition = new THREE.Vector3(0, 0, 0),
-		uDarkColor = new THREE.Color("#07002d"),
-		uLightColor = new THREE.Color("#bca29f"),
-		uLightIntensity = 1.5,
-		uLightRadius = 4.45,
-	}: LandMaterialOptions = {})
-	{
-		super({
-			uniforms: {
-				uTime: { value: 0 },
-				uLightPosition: { value: uLightPosition },
-				uDarkColor: { value: uDarkColor },
-				uLightColor: { value: uLightColor },
-				uLightIntensity: { value: uLightIntensity },
-				uLightRadius: { value: uLightRadius },
-			},
-			vertexShader,
-			fragmentShader,
-			side,
-			transparent: true,
-		})
-	}
+export class LandMaterial extends THREE.ShaderMaterial {
+    constructor({
+        side = THREE.FrontSide,
+        uLightPosition = new THREE.Vector3(0, 0, 0),
+        uDarkColor = new THREE.Color("#07002d"),
+        uLightColor = new THREE.Color("#bca29f"),
+        uLightIntensity = 1.5,
+        uLightRadius = 4.45,
+    }: LandMaterialOptions = {}) {
+        super({
+            uniforms: {
+                uTime: { value: 0 },
+                uLightPosition: { value: uLightPosition },
+                uDarkColor: { value: uDarkColor },
+                uLightColor: { value: uLightColor },
+                uLightIntensity: { value: uLightIntensity },
+                uLightRadius: { value: uLightRadius },
+            },
+            vertexShader,
+            fragmentShader,
+            side,
+            transparent: true,
+        })
+    }
 
-	get uTime(): number
-	{
-		return this.uniforms.uTime.value
-	}
+    get uTime(): number {
+        return this.uniforms.uTime.value
+    }
 
-	set uTime(value: number)
-	{
-		this.uniforms.uTime.value = value
-	}
+    set uTime(value: number) {
+        this.uniforms.uTime.value = value
+    }
 }
