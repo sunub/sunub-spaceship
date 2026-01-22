deb3752af20738918a392f5bf67a1dcbc15f5e23
sunub <bsc5672@gmail.com>
2026-01-22 16:59:01 +0900

    refactor : 코드 스타일을 포맷 Biome 스타일에 맞춰 개선

--- src/widgets/Models/EngineFlame/Shader/EngineFlameMaterial.ts
+++ src/widgets/Models/EngineFlame/Shader/EngineFlameMaterial.ts
@@ -4,48 +4,48 @@ import engineFlameVertex from "./engineFlameVertex"
 import engineflameFragment from "./engineflameFragment"
 
 export class EngineFlameMaterial extends THREE.NodeMaterial {
-	public uTime: UniformNode<number>
-	public uResolution: UniformNode<THREE.Vector2>
-	public uLocalCameraPos: UniformNode<THREE.Vector3>
-	public uMainColor: UniformNode<THREE.Color>
-	public uBaseColor: UniformNode<THREE.Color>
-	public uThrust: UniformNode<number>
-	public uFlameLength: UniformNode<number>
+    public uTime: UniformNode<number>
+    public uResolution: UniformNode<THREE.Vector2>
+    public uLocalCameraPos: UniformNode<THREE.Vector3>
+    public uMainColor: UniformNode<THREE.Color>
+    public uBaseColor: UniformNode<THREE.Color>
+    public uThrust: UniformNode<number>
+    public uFlameLength: UniformNode<number>
 
-	constructor() {
-		super()
+    constructor() {
+        super()
 
-		const vUv = varying(vec2())
-		const vWorldPosition = varying(vec3())
-		const vPosition = varying(vec3())
+        const vUv = varying(vec2())
+        const vWorldPosition = varying(vec3())
+        const vPosition = varying(vec3())
 
-		this.uTime = uniform(0.0)
-		this.uResolution = uniform(
-			new THREE.Vector2(window.innerWidth, window.innerHeight),
-		)
-		this.uLocalCameraPos = uniform(new THREE.Vector3())
-		this.uMainColor = uniform(new THREE.Color(1.0, 0.27, 0.15))
-		this.uBaseColor = uniform(new THREE.Color(0.25, 0.62, 0.79))
-		this.uThrust = uniform(0.0)
-		this.uFlameLength = uniform(0.1)
+        this.uTime = uniform(0.0)
+        this.uResolution = uniform(
+            new THREE.Vector2(window.innerWidth, window.innerHeight),
+        )
+        this.uLocalCameraPos = uniform(new THREE.Vector3())
+        this.uMainColor = uniform(new THREE.Color(1.0, 0.27, 0.15))
+        this.uBaseColor = uniform(new THREE.Color(0.25, 0.62, 0.79))
+        this.uThrust = uniform(0.0)
+        this.uFlameLength = uniform(0.1)
 
-		this.vertexNode = engineFlameVertex(vUv, vWorldPosition, vPosition)
-		this.fragmentNode = engineflameFragment({
-			uTime: this.uTime,
-			uResolution: this.uResolution,
-			uLocalCameraPos: this.uLocalCameraPos,
-			vUv: vUv,
-			vWorldPosition: vWorldPosition,
-			vPosition: vPosition,
-			uMainColor: this.uMainColor,
-			uBaseColor: this.uBaseColor,
-			uThrust: this.uThrust,
-			uFlameLength: this.uFlameLength,
-		})
+        this.vertexNode = engineFlameVertex(vUv, vWorldPosition, vPosition)
+        this.fragmentNode = engineflameFragment({
+            uTime: this.uTime,
+            uResolution: this.uResolution,
+            uLocalCameraPos: this.uLocalCameraPos,
+            vUv: vUv,
+            vWorldPosition: vWorldPosition,
+            vPosition: vPosition,
+            uMainColor: this.uMainColor,
+            uBaseColor: this.uBaseColor,
+            uThrust: this.uThrust,
+            uFlameLength: this.uFlameLength,
+        })
 
-		this.side = THREE.FrontSide
-		this.transparent = true
-		this.depthWrite = false
-		this.blending = THREE.AdditiveBlending
-	}
+        this.side = THREE.FrontSide
+        this.transparent = true
+        this.depthWrite = false
+        this.blending = THREE.AdditiveBlending
+    }
 }
