fa8552b060bc13df3f48e356da4894776b28ce36
sunub <bsc5672@gmail.com>
2026-02-04 09:51:26 +0900

    feat : Entry를 오직 UI 만을 관리하게끔 에셋 로드 로직 제거

--- src/core/Entry/Entry.ts
+++ src/core/Entry/Entry.ts
@@ -1,119 +1,124 @@
 import { EventEmitter } from "node:events"
 import { Vector3 } from "three/webgpu"
-import type Resources from "@/utils/Resources"
-import type { Audio } from "@/widgets/Audio"
+
 import type { Game } from "@/widgets/Game"
-import { entrySources, modelSources, textureSources } from "../../sources"
-import type { GameContext } from "../GameContext"
 import { Atmosphere } from "./model/Atmosphere"
 import { AtmosphereLand } from "./model/AtmosphereLand/AtmosphereLand"
 import { AtmosphereTreeLights } from "./model/AtmosphereTreeLights"
 import { Background } from "./model/Background"
 import { LoadingAnimation } from "./model/LoadingAnimation"
 import { Planet } from "./model/Planet"
+import { inject, injectable } from "inversify"
+import { GAME_CONTEXT } from "../DI/DITypes"
+import type { Lighting } from "@/widgets/Lighting"
+import type { GameContext } from "../GameContext"
 
+@injectable()
 export class Entry extends EventEmitter {
-    private resources: Resources
-    private game: Game
-    private loadingAnimation!: LoadingAnimation
-    private audio: Audio
+    public loadingAnimation!: LoadingAnimation
+    private gameLoader!: HTMLProgressElement | null;
+    private loadingPercentage!: HTMLParagraphElement | null;
+    private assetsRemainingLabel!: HTMLParagraphElement | null;
+    private assetsLoadedCount!: HTMLParagraphElement | null;
+    private loadingText!: HTMLParagraphElement | null;
 
-    constructor(private context: GameContext) {
+    constructor(
+        @inject(GAME_CONTEXT.Game) private game: Game,
+
+        @inject(GAME_CONTEXT.Lighting) private lighting: Lighting,
+    ) {
         super()
-        this.resources = this.context.resources
-        this.game = this.context.game
-        this.audio = this.context.audio
     }
 
-    async init() {
-        await this.resources.load(entrySources)
-
-        await this.settingEntryScene()
-        this.loadRemainingAssets()
-        this.spawn()
-    }
-
-    private async loadRemainingAssets() {
-        let loadedCount = 0
-        const totalCount = modelSources.length + textureSources.length
-
-        const gameLoader = document.getElementById(
-            "game-loader",
-        ) as HTMLProgressElement
-        const loadingPercentage = document.getElementById("loading-percentage")
-        const assetsRemainingLabel = document.getElementById(
-            "assets-remaining-label",
-        )
-        const assetsLoadedCount = document.getElementById("assets-loaded-count")
-        const loadingText = document.getElementById("loading-txt")
-
-        // Initialize display
-        if (assetsLoadedCount) assetsLoadedCount.innerText = `0/${totalCount}`
-        if (assetsRemainingLabel)
-            assetsRemainingLabel.innerText = `Assets Remaining: ${totalCount}`
-
-        const onProgress = () => {
-            loadedCount++
-            const percent = Math.floor((loadedCount / totalCount) * 100)
-
-            if (gameLoader) {
-                gameLoader.removeAttribute("indeterminate")
-                gameLoader.value = percent
-            }
-
-            if (loadingPercentage) {
-                loadingPercentage.innerText = `${percent}%`
-            }
-
-            if (assetsRemainingLabel) {
-                assetsRemainingLabel.innerText = `Assets Remaining: ${totalCount - loadedCount}`
-            }
-
-            if (assetsLoadedCount) {
-                assetsLoadedCount.innerText = `${loadedCount}/${totalCount}`
-            }
-
-            if (loadedCount >= totalCount) {
-                this.loadingAnimation.setLoaded()
-                if (loadingText) loadingText.innerText = "Ready to Launch"
-            }
-        }
-
-        const modelResourcePromise = this.resources.load(
-            modelSources,
-            onProgress,
-        )
-        const textureResourcePromise = this.resources.load(
-            textureSources,
-            onProgress,
-        )
-        const rapierPromise = import("@dimforge/rapier3d-compat")
-        const [, , rapier] = await Promise.all([
-            modelResourcePromise,
-            textureResourcePromise,
-            rapierPromise,
-        ])
-
-        this.context.rapier = rapier
-        await this.context.rapier.init()
-        await this.game.physics.initialize(this.context)
-
-        await this.game.prepareGameObjects()
-
-        this.emit("resourcesLoaded")
-    }
-
-    private spawn() {
-        this.on("resourcesLoaded", async () => {
-            await this.game.settingEnvironment()
-            this.activateEnterButton()
-        })
-    }
-
-    public activateEnterButton() {
+    public enableStartButton(callback: () => Promise<void>) {
         const button = document.getElementById("entry-button")
         if (button) {
             button.removeAttribute("disabled")
+            button.addEventListener("click", async () => {
+                await callback()
+                this.dispose()
+            })
+        }
+    }
+
+
+    public updateProgressUI(current: number, total: number) {
+        if (!this.gameLoader) {
+            this.gameLoader = document.getElementById("game-loader") as HTMLProgressElement
+        }
+        if (!this.loadingPercentage) {
+            this.loadingPercentage = document.getElementById("loading-percentage") as HTMLParagraphElement
+        }
+        if (!this.assetsRemainingLabel) {
+            this.assetsRemainingLabel = document.getElementById("assets-remaining-label") as HTMLParagraphElement
+        }
+        if (!this.assetsLoadedCount) {
+            this.assetsLoadedCount = document.getElementById("assets-loaded-count") as HTMLParagraphElement
+        }
+        if (!this.loadingText) {
+            this.loadingText = document.getElementById("loading-txt") as HTMLParagraphElement
+        }
+
+        const percent = Math.floor((current / total) * 100)
+        if (this.gameLoader) {
+            this.gameLoader.removeAttribute("indeterminate")
+            this.gameLoader.value = percent
+        }
+
+        if (this.loadingPercentage) {
+            this.loadingPercentage.innerText = `${percent}%`
+        }
+
+        if (this.assetsRemainingLabel) {
+            this.assetsRemainingLabel.innerText = `Assets Remaining: ${total - current}`
+        }
+
+        if (this.assetsLoadedCount) {
+            this.assetsLoadedCount.innerText = `${current}/${total}`
+        }
+
+        if (current >= total) {
+            this.loadingAnimation.setLoaded()
+            if (this.loadingText) {
+                this.loadingText.innerText = "Ready to Launch"
+            }
+        }
+    }
+
+    public async setupEntryScene(context: GameContext) {
+        this.lighting.initialize()
+        const object_initializers_promises = this.sceneObjects.map(
+            async (obj) => {
+                await obj.initialize(context)
+                this.game.addGameObject(obj)
+            },
+        )
+        await Promise.all(object_initializers_promises)
+    }
+
+    get sceneObjects() {
+        const objects_pos = new Vector3(0, 39.0, 0)
+        const objects_scale = new Vector3(0.5, 0.5, 0.5)
+
+        this.loadingAnimation = new LoadingAnimation(objects_pos)
+
+        return [
+            this.loadingAnimation,
+            new Planet(objects_pos, objects_scale),
+            new Atmosphere(objects_pos, objects_scale),
+            new AtmosphereLand(objects_pos, objects_scale),
+            new AtmosphereTreeLights(objects_pos, objects_scale),
+            new Background(),
+        ]
+    }
+
+    public removeLoadingScreen() {
+        const loadingScreen = document.getElementById("loading-screen")
+        if (loadingScreen) {
+            loadingScreen.style.opacity = "0"
+            setTimeout(() => {
+                loadingScreen.remove()
+            }, 500)
         }
     }
 
@@ -130,65 +135,5 @@ export class Entry extends EventEmitter {
         }
 
         this.removeAllListeners()
-    }
-
-    private async settingEntryScene() {
-        await this.context.camera.initialize(this.context)
-
-        this.context.lighting.initialize()
-        const object_initializers_promises = this.sceneObjects.map(
-            async (obj) => {
-                await obj.initialize(this.context)
-                this.game.addGameObject(obj)
-            },
-        )
-        await Promise.all(object_initializers_promises)
-
-        this.context.rendering.setPostProcessing()
-        this.game.setupEvents()
-
-        this.createLoadingLoader()
-    }
-
-    public createLoadingLoader() {
-        const button = document.querySelector("#entry-button")
-        if (button) {
-            button.addEventListener("click", () => {
-                this.audio.initAudio()
-                this.audio.play("button")
-                this.audio.createDisplay()
-                this.audio.createPanel()
-                this.audio.handleSoundControl()
-
-                this.game.startGame()
-                this.dispose()
-            })
-        }
-    }
-
-    get sceneObjects() {
-        const objects_pos = new Vector3(0, 39.0, 0)
-        const objects_scale = new Vector3(0.5, 0.5, 0.5)
-
-        this.loadingAnimation = new LoadingAnimation(objects_pos)
-
-        return [
-            this.loadingAnimation,
-            new Planet(objects_pos, objects_scale),
-            new Atmosphere(objects_pos, objects_scale),
-            new AtmosphereLand(objects_pos, objects_scale),
-            new AtmosphereTreeLights(objects_pos, objects_scale),
-            new Background(this.context),
-        ]
-    }
-
-    public removeLoadingScreen() {
-        const loadingScreen = document.getElementById("loading-screen")
-        if (loadingScreen) {
-            loadingScreen.style.opacity = "0"
-            setTimeout(() => {
-                loadingScreen.remove()
-            }, 500)
-        }
     }
 }
